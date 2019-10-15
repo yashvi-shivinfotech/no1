20916decf01e6b2342e5498801e31c32886bb322
yashvi patel <yashvi.shivinfotech@gmail.com>
2019-10-15 14:03:28 +0530

    modified

--- app/src/main/java/com/example/devansh/xmldemo/Main2Activity.java
+++ app/src/main/java/com/example/devansh/xmldemo/Main2Activity.java
@@ -2,12 +2,15 @@ package com.example.devansh.xmldemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
 
+    EditText edt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
+    //hello
 }
